c3b6497c2e22b6ea733639a3ee3fb533d97227b4
mscherer <mark.scherer@spryker.com>
2018-04-28 20:58:09 +0200

    Fix tests.

--- tests/config/routes.php
+++ tests/config/routes.php
@@ -1,9 +1,9 @@
 <?php
 
 use Cake\Core\Plugin;
-use Cake\Routing\Route\DashedRoute;
 use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
+use Cake\Routing\Route\DashedRoute;
 
 Router::plugin('Meta', function (RouteBuilder $routes) {
 	$routes->fallbacks(DashedRoute::class);
